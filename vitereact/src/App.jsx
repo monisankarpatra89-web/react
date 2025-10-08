5792647dd804dd7ada797fa4e44276a9dc711543
Deepak kumar <deepak.kbehera@mindfiresolutions.com>
2025-10-08 13:26:35 +0530

    1st

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Moni from "./Moni"
+
+function App() {
+
+  return (
+    <>
+    <Moni/>
+    <h1>sanker</h1>
+    <h1>Patra</h1>
+    </>
+  )
+}
+
+export default App
---
+++ vitereact/src/Moni.jsx
@@ -0,0 +1,6 @@
+function Moni(){
+    return(
+        <h1>Moni</h1>
+    )
+}
+export default Moni
